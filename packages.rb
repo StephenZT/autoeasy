782e56f7f26c75fd08aa6d64fa3a807e86bb3972
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-18 17:09:57 +0800

    Create packages.rb

---
+++ packages.rb
@@ -0,0 +1,20 @@
+# encoding=utf-8
+#Author: Stephen Zhang
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Config/Supports.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Config/Config.rb"
+
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Assert.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Element.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Page.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/PageRegister.rb"
+
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Action/BrowserAction.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Action/ElementAction.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Action/ElementAssert.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Factories/PageFactory.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Core/Factories/DriverFactory.rb"
+
+require File.expand_path(File.dirname(__FILE__)) + "/Utils/CodeUtil.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Utils/ColorUtil.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Utils/DateUtil.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/Utils/RandomUtil.rb"
