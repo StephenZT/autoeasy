5ed10ab95eb287fb173fc6df2a1041e84672dd13
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-20 11:35:58 +0800

    Update page_steps.rb

--- features/steps/page_steps.rb
+++ features/steps/page_steps.rb
@@ -1,7 +1,12 @@
 # encoding=utf-8
 
 When(/^I visit "([^"]*)" page$/) do |pageName|
-  google_page = PageFactory.getPage(pageName, Configuration.getConfig()['platform'], {:force_refresh=>true})
+  google_page = PageFactory.getPage(pageName, Configuration.getPlatform(), {:force_refresh=>true})
   google_page.action().go_to(google_page.url)
   google_page.action().wait_for_ready(10)
+end
+
+Then(/^I am on the page "([^"]*)"$/) do |pageName|
+  google_page = PageFactory.getPage(pageName, Configuration.getPlatform())
+  google_page.assert().assert_url("contains",google_page.url)
 end
