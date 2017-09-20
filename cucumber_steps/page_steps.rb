4a3a37e500b65c0ca943879d73f49af89f69fa20
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-20 12:47:21 +0800

    Update page_steps.rb

--- cucumber_steps/page_steps.rb
+++ cucumber_steps/page_steps.rb
@@ -1,12 +1,12 @@
 # encoding=utf-8
 
 When(/^I visit "([^"]*)" page$/) do |pageName|
-  google_page = PageFactory.getPage(pageName, Configuration.getPlatform(), {:force_refresh=>true})
-  google_page.action().go_to(google_page.url)
-  google_page.action().wait_for_ready(10)
+  get_page = PageFactory.getPage(pageName, Configuration.getPlatform(), {:force_refresh=>true})
+  get_page.action().go_to(get_page.url)
+  get_page.action().wait_for_ready(10)
 end
 
 Then(/^I am on the page "([^"]*)"$/) do |pageName|
-  google_page = PageFactory.getPage(pageName, Configuration.getPlatform())
-  google_page.assert().assert_url("contains",google_page.url)
+  get_page = PageFactory.getPage(pageName, Configuration.getPlatform())
+  get_page.assert().assert_url("contains",get_page.url)
 end
