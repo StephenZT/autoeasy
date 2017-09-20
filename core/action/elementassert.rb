c371dbe0823738b2bcc7ccc56eaf68ed12b8b556
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-20 10:14:37 +0800

    Update elementassert.rb

--- core/action/elementassert.rb
+++ core/action/elementassert.rb
@@ -9,61 +9,63 @@ module AutoEasy
     class ElementAssert
       #Store ElementAction     
       attr_reader :element_action
+      attr_reader :element_assert
       
-      def initialize(element_act, opt={})
+      def initialize(element_act, opts={})
         @element_action = element_act
+        @element_assert = AutoEasy::Core::Assert.new(opts)
       end
     
       def assert_valid()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is valid.", @element_action.is_valid(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is valid.", @element_action.is_valid(), true, "==")
       end
       
       def assert_invalid()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is invalid.", @element_action.is_valid(), false, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is invalid.", @element_action.is_valid(), false, "==")
       end
       
       def assert_text(assertion, value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] text.", @element_action.get_text(), value, assertion)
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] text.", @element_action.get_text(), value, assertion)
       end
       
       def assert_value(assertion, value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] value.", @element_action.get_value(), value, assertion)
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] value.", @element_action.get_value(), value, assertion)
       end
       
       def assert_selected_text(assertion, value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] selected text.", @element_action.get_selected_text(), value, assertion)
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] selected text.", @element_action.get_selected_text(), value, assertion)
       end
       
       def assert_tag(assertion, value)
-        Assert.assert("Assert element element [#{@element_action.org_element.name}] tag name.", @element_action.get_tag_name(), value, assertion)
+        @element_assert.assert("Assert element element [#{@element_action.org_element.name}] tag name.", @element_action.get_tag_name(), value, assertion)
       end
       
       def assert_option_involved(value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] option [#{value}] involved.", @element_action.get_options_text().include?(value), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] option [#{value}] involved.", @element_action.get_options_text().include?(value), true, "==")
       end
       
       def assert_option_not_involved(value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] option [#{value}] not involved.", @element_action.get_options_text().include?(value), false, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] option [#{value}] not involved.", @element_action.get_options_text().include?(value), false, "==")
       end 
       
       def assert_disabled()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is disabled.", @element_action.is_disabled(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is disabled.", @element_action.is_disabled(), true, "==")
       end
       
       def assert_enabled()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is enabled.", @element_action.is_enabled(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is enabled.", @element_action.is_enabled(), true, "==")
       end
       
       def assert_color(value)
         rgb = @element_action.color()
         hex = ColorUtils.NormalizeColorCode(ColorUtils.ConvertRgbToHex(rgb))
-        Assert.assert("Assert element [#{@element_action.org_element.name}] color.", hex, value, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] color.", hex, value, "==")
       end
       
       def assert_backgroundcolor(value)
         rgb = @element_action.background_color()
         hex = ColorUtils.NormalizeColorCode(ColorUtils.ConvertRgbToHex(rgb))
-        Assert.assert("Assert element [#{@element_action.org_element.name}] color.", hex, value, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] color.", hex, value, "==")
       end
       
       def assert_bordercolor(assertion,value)
@@ -71,17 +73,17 @@ module AutoEasy
         if @element_action.browser.downcase == "firefox"
           rgb.each do |color|
             hex = ColorUtils.NormalizeColorCode(ColorUtils.ConvertRgbToHex(color))
-            Assert.assert("Assert element [#{@element_action.org_element.name}] border color.", hex, value, assertion)
+            @element_assert.assert("Assert element [#{@element_action.org_element.name}] border color.", hex, value, assertion)
           end
         else
           hex = ColorUtils.NormalizeColorCode(ColorUtils.ConvertRgbToHex(rgb))
-          Assert.assert("Assert element [#{@element_action.org_element.name}] border color.", hex, value, assertion)
+          @element_assert.assert("Assert element [#{@element_action.org_element.name}] border color.", hex, value, assertion)
         end
         
       end
       
       def assert_alignment(value)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] alignment.", @element_action.alignment(), value, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] alignment.", @element_action.alignment(), value, "==")
       end
       
       def assert_cssvalue(assertion, attr, value)
@@ -96,37 +98,37 @@ module AutoEasy
           val = "bold" if bold.include?(val.strip)
           expectedValue = "normal" if normal.include?(expectedValue.strip)
           expectedValue = "bold" if bold.include?(expectedValue.strip)
-          Assert.assert("Assert element css [#{attr}] value.", val, expectedValue, "==")
+          @element_assert.assert("Assert element css [#{attr}] value.", val, expectedValue, "==")
         elsif ["color", "background-color","outline-color"].include?(attr.downcase)
           rgb =  @element_action.css_value(attr)
           hex = ColorUtils.NormalizeColorCode(ColorUtils.ConvertRgbToHex(rgb))
-          Assert.assert("Assert element [#{@element_action.org_element.name}] css [#{attr}] value.", hex, value, assertion)
+          @element_assert.assert("Assert element [#{@element_action.org_element.name}] css [#{attr}] value.", hex, value, assertion)
         else
-          Assert.assert("Assert element [#{@element_action.org_element.name}] css [#{attr}] value.", @element_action.cssvalue(attr), value, assertion)
+          @element_assert.assert("Assert element [#{@element_action.org_element.name}] css [#{attr}] value.", @element_action.cssvalue(attr), value, assertion)
         end
       end
       
       def assert_checked()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is checked.", @element_action.is_checked(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is checked.", @element_action.is_checked(), true, "==")
       end
 
       def assert_notchecked()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is not checked.", @element_action.is_not_checked(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is not checked.", @element_action.is_not_checked(), true, "==")
       end
       
       def assert_focused()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is focused.", @element_action.is_focused(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is focused.", @element_action.is_focused(), true, "==")
       end
       
       def assert_notfocused()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is not focused.", @element_action.is_not_focused(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is not focused.", @element_action.is_not_focused(), true, "==")
       end
       
       def assert_link_navigation(assertion, value, time)
         @element_action.click()
         browser = AutoEasy::Core::BrowserAction.new(@element_action.driver)
         browser.wait_for_ready(time)
-        Assert.assert("Assert element [#{@element_action.org_element.name}] link navigation #{value}.", browser.get_url().downcase, value.downcase, assertion)
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] link navigation #{value}.", browser.get_url().downcase, value.downcase, assertion)
         browser.go_back()
       end
       
@@ -136,16 +138,16 @@ module AutoEasy
         browser = AutoEasy::Core::BrowserAction.new(@element_action.driver)
         browser.wait_for_ready(time)
         browser.switch_to_last_window
-        Assert.assert("Assert element [#{@element_action.org_element.name}] link popup #{value}.", browser.get_url().downcase, value.downcase, assertion)
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] link popup #{value}.", browser.get_url().downcase, value.downcase, assertion)
         browser.close_window()
       end
       
       def assert_viewable()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is viewable.", @element_action.is_viewable(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is viewable.", @element_action.is_viewable(), true, "==")
       end
       
       def assert_notviewable()
-        Assert.assert("Assert element [#{@element_action.org_element.name}] is not viewable.", @element_action.is_not_viewable(), true, "==")
+        @element_assert.assert("Assert element [#{@element_action.org_element.name}] is not viewable.", @element_action.is_not_viewable(), true, "==")
       end
       
     end
