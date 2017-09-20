148b432e71c555931b30ef962346c4dbdc32339f
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-20 10:03:30 +0800

    Update config.rb

--- core/config/config.rb
+++ core/config/config.rb
@@ -1,21 +1,13 @@
 require File.expand_path(File.dirname(__FILE__)) + "/Supports.rb"
+require 'singleton'
 
 module AutoEasy
   module Core
     class Config
+      include Singleton
       #current settings
-      attr_reader :setting
-      
-      def initialize(opt={})
-        @setting = Hash.new
-        #Set default settings
-        @setting['environment'] = AutoEasy::Core::Supports::ENVIRONMENTS[0]
-        @setting['platform'] = AutoEasy::Core::Supports::PLATFORMS[0]
-        @setting['browser'] = AutoEasy::Core::Supports::BROWSERS[0]
-        @setting['device'] = AutoEasy::Core::Supports::DEVICES[0]
-        @setting['loglevel'] = AutoEasy::Core::Supports::LOGLEVELS[0]
-      end
-      
+      attr_accessor :setting
+       
       def setEnvironment(env)
         @setting['environment'] = env
       end
@@ -36,6 +28,28 @@ module AutoEasy
         @setting['loglevel'] = loglevel
       end
       
+      def setConfig(key,value)
+        @setting[key] = value
+      end
+      
+      def getConfig()
+        return @setting
+      end
+      
+      def setDefault()
+        #Set default settings
+        @setting = Hash.new if @setting == nil
+        @setting['environment'] =  AutoEasy::Core::Supports::ENVIRONMENTS[0]
+        @setting['platform'] = AutoEasy::Core::Supports::PLATFORMS[0]
+        @setting['browser'] = AutoEasy::Core::Supports::BROWSERS[0]
+        @setting['device'] = AutoEasy::Core::Supports::DEVICES[0]
+        @setting['loglevel'] = AutoEasy::Core::Supports::LOGLEVELS[0]
+      end
+      
     end
   end
 end
+
+Configuration ||= AutoEasy::Core::Config.instance
+Configuration.setDefault()
+
