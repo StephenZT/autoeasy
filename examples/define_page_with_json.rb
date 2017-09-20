d068339e4cd864bee0e2fca8c49d9304b4f1208c
StephenZT <31845720+StephenZT@users.noreply.github.com>
2017-09-20 10:08:36 +0800

    Update define_page_with_json.rb

--- examples/define_page_with_json.rb
+++ examples/define_page_with_json.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__)) + "/../Packages.rb"
-require 'selenium-webdriver'
 
 #Page definition
 page_data = {'searchInput'=> {
@@ -27,21 +26,19 @@ begin
   google_page = PageFactory.getPage('google_first_page','desktop')
   google_page.action().go_to(google_page.url)
   google_page.action().wait_for_ready(10)
-  
+ 
   google_page.action().open_new_window()
   google_page.action().close_window()
   
+  google_page.assert().assert_url("==","http://www.google.com")
   google_page.getElement('invalidEl').assert().assert_invalid()
-
-  
   google_page.getElement('searchInput').action().fill('abc')
   google_page.getElement('searchButton').action().click()
-
   google_page.getElement('searchInput').assert().assert_value("==",'abc')
   google_page.getElement('searchInput').action().fill('abcdefagasdgag212314325253')
   google_page.getElement('searchButton').action().click()
   google_page.getElement('searchInput').assert().assert_value("==",'abc')
-  
+  LoggerTrace.assert_no_error_in_log()
   sleep(3)
 
   driver.quit
