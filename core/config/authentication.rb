4663ccb678d8c3097fcff95ac1514bfde794eba6
unknown <szhang2@.nextestate.com>
2017-11-22 17:23:34 +0800

    Update

--- core/config/authentication.rb
+++ core/config/authentication.rb
@@ -1,10 +1,15 @@
 # encoding=utf-8
 #Author: Stephen Zhang
-
+# Authention definition for rest/soap service call
 module AutoEasy
   module Core
     class Authentication
-      
+      attr_reader :domain, :username, :password
+
+      def initialize(domain="", username, password)
+        @domain, @username, @password = domain, username, password
+      end
+
     end
   end
 end
